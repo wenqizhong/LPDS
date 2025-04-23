14e2b8d664307896fd09055cd61d32360981ae79
wenqizhong <87892823+wenqizhong@users.noreply.github.com>
2025-04-23 16:09:11 +0800

    Add files via upload

---
+++ train_iter.py
@@ -0,0 +1,74 @@
+#coding=utf8
+import numpy as np
+import os
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import time
+import argparse
+
+from data_utils import getTrainVal_loader, getTest_loader
+from model.classmodel import ClassModle
+import trainer_iter
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    np.random.seed(12)
+
+
+    parser.add_argument('--train_dataset_dir', type=str, default='/dataset/saliency4asd')
+    parser.add_argument('--train_stimuli_dir', type=str, default='Images', help='images dir')
+    parser.add_argument('--train_scanpath_dir', type=str, default='train', help='scanpath dir')
+    parser.add_argument('--checkpoint', type=str, default='checkpoints/saliency4asd', help='the path to save model')
+    parser.add_argument('--logs', type=str, default='logs/saliency4asd/scanformer', help='the path to save training logs')
+    parser.add_argument('--model_name', type=str, default='scanformer')
+
+    parser.add_argument('--batch_size', type=int, default=1, help='input batch size for training, only supports 1')
+    parser.add_argument('--lr', type=float, default=0.0001, help='learning rate for training')
+    parser.add_argument('--momentum', type=float, default=0.9, help='momentum for training')
+    parser.add_argument('--decay', type=float, default=0.0005, help='weight decay for training')
+    parser.add_argument('--epochs', type=int, default=20, help='epoch for training')
+
+
+    parser.add_argument('--train_p', type=str, default='people/p_train_1', help='people use for train')
+    parser.add_argument('--test_p', type=str, default='people/p_test_1', help='people use for test')
+    parser.add_argument('--discrimination_path', type=str, help='path of the discrimination score')
+    parser.add_argument('--gpu', default=True)
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() and args.gpu else "cpu")
+
+    # need_img = 'score-fisher_gender30p/fisher_ampltude_img'
+    # need_img = None
+    dataloaders = getTrainVal_loader(args.train_dataset_dir, args.train_stimuli_dir, args.train_scanpath_dir, train_p=args.train_p, need_images=need_img, batch_s=args.batch_size)
+
+    if not os.path.exists("%s/path-%s" %(args.checkpoint, args.model_name)):
+        os.mkdir("%s/path-%s" %(args.checkpoint, args.model_name))
+
+    model = ClassModle()
+    
+    print('model done')
+    model.to(device)
+    optimizer_ft = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.decay,
+                             nesterov=True)
+
+    criterion = torch.nn.BCELoss()
+    print("Begin train, Device: {}".format(device))
+
+    disc_dict = {}
+    with open(args.discrimination_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if ':' in line:
+                key, value = line.split(':', 1)
+                disc_dict[key.strip()] = value.strip()
+
+    trainer.train_model(args, model, device, dataloaders, criterion, optimizer_ft, num_epochs=args.epochs, disc_dict=disc_dict,
+                check_point=1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ trainer_iter.py
@@ -0,0 +1,69 @@
+import os
+import torch
+torch.autograd.set_detect_anomaly(True)
+import time
+from torch.utils.tensorboard import SummaryWriter
+
+
+def train_model(args, model, device, dataloaders, criterion, optimizer, num_epochs, disc_dict=None, times=None, check_point=1):
+    if args.train_p is None:
+        p_save = 'train'
+    else:
+        p_save = os.path.basename(args.train_p)[2:]
+        
+    writer = SummaryWriter(os.path.join(args.logs, (('%s-'%p_save) + time.strftime("%m%d-%H%M"))))
+    model.train()
+    valid_loss_min = float('inf')
+
+    for epoch in range(num_epochs):
+        print('dataset:{}, val split:{}'.format(os.path.basename(args.train_dataset_dir), os.path.basename(args.train_p)))
+        print("Epoch {}/{}".format(epoch + 1, num_epochs))
+        print("-" * 20)
+
+        for phase in ['train', 'val']:
+            running_loss = 0
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            cnt = 0
+            for iteration, (img_name, img, scanpaths, labels, trans_factor) in enumerate(dataloaders[phase]):
+
+                duration = trans_factor[4]
+                img = img.to(device)
+                scanpaths = scanpaths.to(device)
+                duration = duration.to(device)
+                labels = labels.to(device)
+
+                with torch.autograd.set_grad_enabled(phase == 'train'):
+                    outputs = model(img, scanpaths)
+                    if disc_dict is not None:
+                        stimuli_weights = disc_dict[img_name]
+                        loss_per_stimuli = criterion(outputs, labels)
+                        weighted_loss = loss_per_stimuli * stimuli_weights
+                        loss = weighted_loss.mean() 
+                    else:
+                        loss = criterion(outputs, labels)
+
+
+                if phase == 'train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                cnt = cnt + 1
+                if cnt == len(dataloaders[phase]):
+                    print("\r {} Complete: {:.2f}".format(phase, cnt / len(dataloaders[phase])), end='\n')
+                else:
+                    print("\r {} Complete: {:.2f}".format(phase, cnt / len(dataloaders[phase])), end="")
+
+                running_loss += loss.item() * img.size(0)
+
+            epoch_loss = running_loss / len(dataloaders[phase])
+
+
+            print("{} Loss: {}".format(phase, epoch_loss))
+        save_path = os.path.join(args.checkpoint, ('path-' + args.model_name))
+        if (epoch+1) % check_point == 0:
+            torch.save(model.state_dict(), (os.path.join(save_path, ('model-%s_%s.pth'%(str(epoch+1),(p_save))))))
